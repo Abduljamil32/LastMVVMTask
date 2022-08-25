ae6d1f91b972967e9754ddcdd59395073a5e001e
co.abu <anizomxoja@gmail.com>
2022-08-25 12:01:59 +0500

    Initial Commit

---
+++ LastMVVMTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LastMVVMTask
+//
+//  Created by Avaz Mukhitdinov on 25/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
